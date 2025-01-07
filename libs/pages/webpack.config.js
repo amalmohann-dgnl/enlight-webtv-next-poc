37f3c9454f3f3244f655b6cfae11721f5ad3307d
amalmohann <amal.mohan@diagnal.com>
2025-01-07 12:54:52 +0530

    set the build configuration

--- libs/pages/webpack.config.js
+++ libs/pages/webpack.config.js
@@ -16,11 +16,52 @@ module.exports = (config, context) => {
       outputModule: true, // Enable output as ES modules
     },
     resolve: {
-      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.scss', '.css'], // Added .scss and .css
     },
     externals: {
       react: 'react',
       'react-dom': 'react-dom',
+    },
+    module: {
+      rules: [
+        // Babel loader for TypeScript and JavaScript files
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                '@babel/preset-env', // Transforms ES6+ syntax
+                '@babel/preset-react', // Transforms JSX
+                '@babel/preset-typescript', // Supports TypeScript
+              ],
+            },
+          },
+        },
+        // SCSS loader with CSS Modules support
+        {
+          test: /\.scss$/,
+          use: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: {
+                  auto: true, // Enable CSS Modules for files ending with .module.scss
+                  localIdentName: '[name]__[local]___[hash:base64:5]', // Customize class naming
+                },
+              },
+            },
+            'sass-loader',
+          ],
+        },
+        // CSS loader for global CSS
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
     },
   });
 };
