50dd8dd9ab35e71e44ccf7068d51cc4d12666423
amalmohann <amal.mohan@diagnal.com>
2025-01-08 11:34:05 +0530

    complete the splash navigation

--- apps/illicoPlus/src/app/page.tsx
+++ apps/illicoPlus/src/app/page.tsx
@@ -1,8 +1,20 @@
 import styles from './page.module.scss';
 import { SplashMediaType } from '@enlight-webtv/models'
+// import {  } from '@enlight-webtv/services'
 import { Splash } from '@enlight-webtv/pages'
+import { redirect } from 'next/navigation'
+import { Suspense, use } from 'react';
 
 export default function Index() {
+  const RenderApplication = () => {
+    const isBootComplete = use(fetchBootFn());
+    const isAuthenticated = true;
+    if (isAuthenticated) {
+      redirect('/home')
+    } else {
+      redirect('/login')
+    }
+  }
 
   /*
    * Replace the elements be
@@ -10,7 +22,19 @@ export default function Index() {
    */
   return (
     <div className={styles.page}>
-      <Splash src={'/images/splash.png'} isMediaSplash={false} mediaType={SplashMediaType.IMAGE} showLoader={true} showVersion={true} bgColor={''} />
-    </div>
+      <Suspense
+        fallback={
+        <Splash src={'/images/splash.png'} isMediaSplash={false} mediaType={SplashMediaType.IMAGE} showLoader={true} showVersion={true} bgColor={''} />
+        }>
+        <RenderApplication />
+      </Suspense>
+        </div>
   );
 }
+
+function fetchBootFn(): import("react").Usable<unknown> {
+  return new Promise((resolve)=> setTimeout(() => {
+    return resolve(true);
+  }, 5000))
+}
+
