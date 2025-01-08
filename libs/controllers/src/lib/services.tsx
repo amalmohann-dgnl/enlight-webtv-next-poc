36271176bb1f9dfa4cdb48c7966bab5c1e4449ec
amalmohann <amal.mohan@diagnal.com>
2025-01-08 16:19:56 +0530

    added controllers library

---
+++ libs/controllers/src/lib/services.tsx
@@ -0,0 +1,9 @@
+export function Services() {
+  return (
+    <div>
+      <h1>Welcome to Services!</h1>
+    </div>
+  );
+}
+
+export default Services;
---
+++ libs/controllers/webpack.config.js
@@ -0,0 +1,35 @@
+const { merge } = require('webpack-merge');
+const { withNx } = require('@nrwl/webpack');
+const path = require('path');
+
+module.exports = (config, context) => {
+  const isProduction = context.configurationName === 'production';
+  return merge(withNx(config), {
+    mode: isProduction ? 'production' : 'development',
+    entry: path.resolve(__dirname, 'src/index.ts'), // Explicitly define the entry point
+    output: {
+      libraryTarget: 'module', // Use ES6 module format
+      filename: 'libs/controllers.mjs', // Output filename with .mjs extension
+      chunkFormat: 'module', // Ensure chunks are in ES module format
+    },
+    experiments: {
+      outputModule: true, // Enable output as ES modules
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    },
+    externals: {
+      react: 'react',
+      'react-dom': 'react-dom',
+    },
+  });
+};
