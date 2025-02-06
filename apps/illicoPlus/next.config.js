a28c66a29937646e8cd9dc1415f3be27ef7e0add
amalmohann <amal.mohan@diagnal.com>
2025-02-06 15:27:06 +0530

    transpiled the uuid package

--- apps/illicoPlus/next.config.js
+++ apps/illicoPlus/next.config.js
@@ -16,6 +16,7 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  transpilePackages: ['uuid'],
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
